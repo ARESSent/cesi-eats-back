28b6e9a2c7ad4c9d352888aa317cdc202725e33f
DELAPORTE Alexis <alexis.brice.delaporte@gmail.com>
2024-04-05 09:33:25 +0200

    kick header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Header from "./components/Header"; 
 import Footer from "./components/Footer";
 import Browse from "./Browse";
 import Cart from "./Cart"; 
@@ -16,7 +15,6 @@ function App() {
   return (
     <>
       <Router>
-        <Header />
         <main>
           <Routes>
               <Route path="/" element={<Browse />} />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
-      Hello Footer
+      <nav>
+        <ul>
+          <li>
+            <Link to="/Browse">Browse</Link>
+          </li>
+          <li>
+            <Link to="/Cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/Profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/LogIn">Log In</Link>
+          </li>
+          <li>
+            <Link to="/SignIn">Sign In</Link>
+          </li>
+        </ul>
+      </nav>     
     </footer>
   );
 } 
